8de4760cdadb452d846c699a0969e97fe2278218
Alex <alexballen53@gmail.com>
2023-09-01 21:29:57 -0500

    Se implementan metodo de UPDATE, DELETE, SEARCH Y FILTER CON STREAM API.

--- seguimientoGastosConsignaTrabajoIntegrador/src/dao/ExpenseDao.java
+++ seguimientoGastosConsignaTrabajoIntegrador/src/dao/ExpenseDao.java
@@ -2,7 +2,9 @@ package dao;
 
 import dao.dto.ExpenseDto;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface ExpenseDao {
     //CRUD
@@ -14,7 +16,22 @@ public interface ExpenseDao {
     List<ExpenseDto> getAll();
 
     //UPDATE
+    void updateAll (ExpenseDto expenseDto);
+    void updateName (ExpenseDto expenseDto);
+    void costUpdate (ExpenseDto expenseDto);
+    void categoryUpdate (ExpenseDto expenseDto);
+    void descriptionUpdate (ExpenseDto expenseDto);
 
     //DELETE
+    void delete (ExpenseDto expenseDto);
+
+    //SEARCH
+    List<ExpenseDto> searchName (String name);
+    List<ExpenseDto> searchCategory (String category);
+    List<ExpenseDto> searchDescription (String description);
+
+    //FILTERS STREAM-API
+    List<ExpenseDto> filterExpensesOfTheDay ();
+    Map<String, Object> filterExpensesForTheWeek (LocalDate startDate, LocalDate endDate);
 
 }
