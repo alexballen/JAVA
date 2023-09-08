897b6ad4f8f4b21724f20f42befda30dfecf980f
Alex <alexballen53@gmail.com>
2023-09-07 19:15:47 -0500

    Se modifica la interface y se añaden dos metodo mas que ejecutan la logica de la interface de uaurio, se modulariza.

---
+++ seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/interfaces/OptionMenuInt.java
@@ -0,0 +1,9 @@
+package expenseServices.interfaces;
+
+import java.util.Scanner;
+
+public interface OptionMenuInt {
+    boolean repit ();
+    void menu (int categoryOption);
+    void menuFunctions(int categoryOption);
+}
