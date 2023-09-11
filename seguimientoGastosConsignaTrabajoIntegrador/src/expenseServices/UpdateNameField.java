a6e41328bb5d2b936810b7819e7e85f7b27fb9a6
Alex <alexballen53@gmail.com>
2023-09-11 18:54:07 -0500

    Se refactoriza la clase que actualiza el campo nombre donde se aplica la reutilizacion de codigo, y donde en el primer bloque realiza la busqueda por id en la base de datos del gasto y si encuentra entonces realiza en el segundo bloque la actualizacion del campo.

--- seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/UpdateNameField.java
+++ seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/UpdateNameField.java
@@ -1,47 +1,33 @@
 package expenseServices;
 
 import dao.dto.ExpenseDto;
+import expenseServices.interfaces.CurrentDateTimeInt;
+import expenseServices.interfaces.ExpenseNameInt;
 import expenseServices.interfaces.SearchExpenseDBInt;
 import expenseServices.interfaces.UpdateNameFieldInt;
 
-import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.Scanner;
 
 public class UpdateNameField implements UpdateNameFieldInt {
     @Override
     public ExpenseDto updateName() {
         SearchExpenseDBInt searchExpenseDBInt = new SearchExpenseDB();
+        ExpenseNameInt expenseNameInt = new ExpenseName();
+        CurrentDateTimeInt currentDateTimeInt = new CurrentDateTime();
+
         Map<String, Object> result = searchExpenseDBInt.searchExpense();
 
         ExpenseDto expenseDto = (ExpenseDto) result.get("expenseDto");
         boolean foundRecord = (boolean) result.get("foundRecord");
-        Scanner scanner = (Scanner) result.get("Scanner");
 
         if (foundRecord) {
-            //ACTUALIZAR CON EL NUEVO VALOR DEL NOMBRE DEL GASTO
-            String expenseName;
-            do {
-                System.out.println("Ingresa el nuevo nombre del gasto: ");
-                expenseName = scanner.nextLine().trim();
-
-                if (expenseName.isEmpty()) {
-                    System.out.println("El campo nombre no puede estar vacío. Por favor, ingresa un nombre válido.");
-                } else if (expenseName.length() < 5) {
-                    System.out.println("Debes ingresar un nombre de gasto con al menos 5 caracteres.");
-                } else if (expenseName.length() >= 50) {
-                    System.out.println("Debes ingresar un nombre de gasto con menos de 50 caracteres.");
-                } else if (!expenseName.matches("^[a-zA-Z ]+$")) {
-                    System.out.println("El nombre del gasto debe contener solo letras y espacios.");
-                }
-                expenseDto.setExpenseName(expenseName);
-            } while (expenseName.isEmpty() || expenseName.length() < 5 || expenseName.length() >= 50 || !expenseName.matches("^[a-zA-Z ]+$"));
-
+            //CAMBIAR EL NOMBRE DE UN GASTO
+            String expenseName = expenseNameInt.expenseName();
+            expenseDto.setExpenseName(expenseName);
             System.out.println("Campo actualizado con exito¡");
 
             //SE INGRESA LA FECHA Y HORA ACTUALIZADAS AUTOMATICAMENTE
-            LocalDateTime currentDateTimeName  = LocalDateTime.now();
-            expenseDto.setDateTimeExpense(currentDateTimeName);
+            expenseDto.setDateTimeExpense(currentDateTimeInt.currentDateTime());
         }
         return expenseDto;
     }
