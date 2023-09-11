a514b7ecf04b71f279d85a0b6ac3b043b9412351
Alex <alexballen53@gmail.com>
2023-09-11 18:53:48 -0500

    Se refactoriza la clase que actualiza el campo descripcion donde se aplica la reutilizacion de codigo, y donde en el primer bloque realiza la busqueda por id en la base de datos del gasto y si encuentra entonces realiza en el segundo bloque la actualizacion del campo.

--- seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/UpdateDescriptionField.java
+++ seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/UpdateDescriptionField.java
@@ -1,45 +1,27 @@
 package expenseServices;
 
 import dao.dto.ExpenseDto;
-import expenseServices.interfaces.SearchExpenseDBInt;
-import expenseServices.interfaces.UpdateDescriptionFieldInt;
+import expenseServices.interfaces.*;
 
-import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.Scanner;
-
 public class UpdateDescriptionField implements UpdateDescriptionFieldInt {
     @Override
     public ExpenseDto updateDescription() {
         SearchExpenseDBInt searchExpenseDBInt = new SearchExpenseDB();
+        ExpenseDescriptionInt expenseDescriptionInt = new ExpenseDescription();
+        CurrentDateTimeInt currentDateTimeInt = new CurrentDateTime();
+
         Map<String, Object> result = searchExpenseDBInt.searchExpense();
 
         ExpenseDto expenseDto = (ExpenseDto) result.get("expenseDto");
         boolean foundRecord = (boolean) result.get("foundRecord");
-        Scanner scanner = (Scanner) result.get("Scanner");
 
         if (foundRecord) {
-            //ACTUALIZAR CON EL NUEVO VALOR DE LA DESCRIPCION
-            String descriptionOfExpenses;
-            do {
-                System.out.println("Ingresa una nueva descripcion de tu gasto: ");
-                descriptionOfExpenses = scanner.nextLine().trim();
-
-                if (descriptionOfExpenses.isEmpty()) {
-                    System.out.println("El campo descripcion no puede estar vacío. Por favor, ingresa una descripcion válida.");
-                } else if (descriptionOfExpenses.length() < 5) {
-                    System.out.println("Debes ingresar una descripción de gasto con al menos 5 caracteres.");
-                } else if (descriptionOfExpenses.length() >= 50) {
-                    System.out.println("Debes ingresar una descripción de gasto con menos de 50 caracteres.");
-                }
-                expenseDto.setExpenseDescription(descriptionOfExpenses);
-            } while (descriptionOfExpenses.isEmpty() || descriptionOfExpenses.length() < 5 || descriptionOfExpenses.length() >= 50);
+            String descriptionOfExpenses = expenseDescriptionInt.expenseDescription();
+            expenseDto.setExpenseDescription(descriptionOfExpenses);
 
             //SE INGRESA LA FECHA Y HORA ACTUALIZADAS AUTOMATICAMENTE
-            LocalDateTime currentDateTimeDescription  = LocalDateTime.now();
-            expenseDto.setDateTimeExpense(currentDateTimeDescription);
-
-            System.out.println("Campo actualizado con exito¡");
+            expenseDto.setDateTimeExpense(currentDateTimeInt.currentDateTime());
         }
         return expenseDto;
     }
