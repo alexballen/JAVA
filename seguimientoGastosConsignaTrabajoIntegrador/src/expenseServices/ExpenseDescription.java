3e5fea67d41de9f111977b87491cf531337d1717
Alex <alexballen53@gmail.com>
2023-09-11 18:43:23 -0500

    Se crea clase de descripcion de gasto, que retorna un string con el valor ingresado, que implementa una interface y su metodo.

---
+++ seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/ExpenseDescription.java
@@ -0,0 +1,28 @@
+package expenseServices;
+
+import expenseServices.interfaces.ExpenseDescriptionInt;
+import expenseServices.interfaces.ScannerProviderInt;
+
+import java.util.Scanner;
+public class ExpenseDescription implements ExpenseDescriptionInt {
+    @Override
+    public String expenseDescription() {
+        ScannerProviderInt scannerProviderInt = new ScannerProvider();
+        Scanner scanner = scannerProviderInt.getScannerInstance();
+
+        String descriptionOfExpenses;
+        do {
+            System.out.println("Ingresa una nueva descripcion de tu gasto: ");
+            descriptionOfExpenses = scanner.nextLine().trim();
+
+            if (descriptionOfExpenses.isEmpty()) {
+                System.out.println("El campo descripcion no puede estar vacío. Por favor, ingresa una descripcion válida.");
+            } else if (descriptionOfExpenses.length() < 5) {
+                System.out.println("Debes ingresar una descripción de gasto con al menos 5 caracteres.");
+            } else if (descriptionOfExpenses.length() >= 50) {
+                System.out.println("Debes ingresar una descripción de gasto con menos de 50 caracteres.");
+            }
+        } while (descriptionOfExpenses.isEmpty() || descriptionOfExpenses.length() < 5 || descriptionOfExpenses.length() >= 50);
+        return  descriptionOfExpenses;
+    }
+}
