20de30e35a21894baafc9c0e7fe3e84dd268fbfe
Alex <alexballen53@gmail.com>
2023-09-12 12:58:41 -0500

    Se refactoriza la clase, para aplicar la reurilizacion de codigo y solo llamar a el metodo que ya realiza esta funcionalidad.

--- seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/SearchExpenseName.java
+++ seguimientoGastosConsignaTrabajoIntegrador/src/expenseServices/SearchExpenseName.java
@@ -3,36 +3,18 @@ package expenseServices;
 import dao.ExpenseDao;
 import dao.dto.ExpenseDto;
 import dao.impl.ExpenseDaoImplH2;
+import expenseServices.interfaces.ExpenseNameInt;
 import expenseServices.interfaces.SearchExpenseNameInt;
 
 import java.util.List;
-import java.util.Scanner;
-
 public class SearchExpenseName implements SearchExpenseNameInt {
     @Override
     public void searchExpenseByName() {
         ExpenseDao expenseDao = new ExpenseDaoImplH2();
-        Scanner scanner = new Scanner(System.in);
+        ExpenseNameInt expenseNameInt = new ExpenseName();
 
         //BUSQUEDA DE GASTO POR PALABRA CONTENIDA EN EL NOMBRE DEL GASTO
-        String expenseName;
-        do {
-            System.out.println("Ingresa el nombre del gasto que deseas encontrar: ");
-            expenseName = scanner.nextLine().trim();
-
-            if (expenseName.isEmpty()) {
-                System.out.println("El campo de busqueda nombre no puede estar vacío. Por favor, ingresa un nombre válido.");
-            } else if (expenseName.length() < 5) {
-                System.out.println("Debes ingresar un nombre de gasto con al menos 5 caracteres.");
-            } else if (expenseName.length() >= 50) {
-                System.out.println("Debes ingresar un nombre de gasto con menos de 50 caracteres.");
-            } else if (!expenseName.matches("^[a-zA-Z ]+$")) {
-                System.out.println("El nombre del gasto debe contener solo letras y espacios.");
-            }
-
-        } while (expenseName.isEmpty() || expenseName.length() < 5 || expenseName.length() >= 50 || !expenseName.matches("^[a-zA-Z ]+$"));
-
-        List<ExpenseDto> filterByName = expenseDao.searchName(expenseName);
+        List<ExpenseDto> filterByName = expenseDao.searchName(expenseNameInt.expenseName());
 
         for (ExpenseDto expense: filterByName) {
             System.out.println("ID: " + expense.getExpense_id());
